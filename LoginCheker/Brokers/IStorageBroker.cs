3df8a7de2e43ed715142960a003493d8a32157a1
RomaDev111 <K_ramazon12@mail.ru>
2024-03-12 06:18:07 +0500

    Add project files.

---
+++ LoginCheker/Brokers/IStorageBroker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoginCheker.Models;
+
+namespace LoginCheker.Brokers
+{
+    internal interface IStorageBroker
+    {
+        Credential[] GetAllCredentials();
+    }
+}
---
+++ LoginCheker/Brokers/StorageBroker.cs
@@ -0,0 +1,14 @@
+﻿using LoginCheker.Models;
+
+namespace LoginCheker.Brokers
+{
+    internal class StorageBroker : IStorageBroker
+    {
+        private static Credential[] credentials =
+        {
+            new Credential { UserName = "Roma", Password = "123456" },
+            new Credential { UserName = "Feruz", Password = "654321" }
+        };
+        public Credential[] GetAllCredentials() => credentials;
+    }
+}
